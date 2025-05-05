12de45d20b2b9f40e64f28d87fdbb35a72ffe485
BT_2S14_29 <gabriellej34@nycstudents.net>
2025-05-05 10:51:37 -0400

    5/5/25 - Created class for player

--- src/Johnny.java
+++ src/Johnny.java
@@ -1,7 +1,37 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Johnny {
-    private Object imageFileName;
+    private String imageFileName;
+    private BufferedImage image;
+    private boolean show;
+
 
     public Johnny () {
         this.imageFileName = "cardboardknife.png";
+        this.show = true;
+        this.image = readImage();
+    }
+
+    public BufferedImage getImage() {
+
+        return image;
+    }
+
+    public BufferedImage readImage() {
+        try {
+            BufferedImage image;
+            if (show) {
+                image = ImageIO.read(new File(imageFileName));
+            }
+            return null;
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
+
     }
 }
