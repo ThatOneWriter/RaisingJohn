a9196720e371023ff792dc4f4c721a4e019ae371
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-16 10:51:51 -0400

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,21 +1,51 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.util.Timer;
 
 public class MainFrame extends JFrame implements Runnable {
 
+    private DrawPanel p;
     private Thread windowThread;
+    private Johnny j;
+    private int count = 2;
 
     public MainFrame(String display) {
         super(display);
+        int n = 2;
+        int frameWidth = 800;
+        int frameHeight = 800;
+        p = new DrawPanel();
+        j = new Johnny();
+        this.addKeyListener(p);
+        this.add(p);
+        this.add(j);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Hello.");
+        panel.add(label);
+        this.add(panel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(frameWidth, frameHeight);
+        this.setLocation(500, 10);
+        this.setVisible(true);
         startThread();
+        if (count > 2) {
+            label.getSize();
+            n *= 2;
+            count = 0;
+        }
+
+
     }
 
-    private void startThread() {
+    public void startThread() {
         windowThread = new Thread(this);
         windowThread.start();
     }
 
-    @Override
     public void run() {
-        
+        while (true) {
+            p.repaint();
+            count++;
+            System.out.println(count);
+        }
     }
 }
