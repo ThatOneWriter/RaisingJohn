e96f82ff4395cf7768a30ea61c7f5319bab9c741
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-19 10:52:10 -0400

    I attempted to make an intro message to the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        String message = "\n" +
+                "\n" +
+                "               Introduction Text:" +
+                "        You have been transported to an interactive story mode game where you the user\n" +
+                "        are a teacher trying to navigate their way working at an extremely elite \n" +
+                "        superhero high school. \n" +
+                " ";
+        long speed = 200;
+
+        Introduction.typewrittenIntro(message, speed);
+        
         MainFrame mainFrame = new MainFrame("Raising Johnny");
+
+        
     }
 }
