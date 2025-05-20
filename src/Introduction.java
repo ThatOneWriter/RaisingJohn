dcb97ec10e4a2110f830dac14b46dd3ce4862a5d
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-20 10:50:51 -0400

    Finished Typewriter class

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,7 +1,15 @@
 public class Introduction {
     public static void typewrittenIntro(String text, long speed) {
         for (int i = 0; i < text.length(); i++) {
+            char print = text.charAt(i);
+            try {
+                Thread.sleep(speed);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(print);
 
         }
-        }
+
+    }
 }
