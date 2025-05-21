898af0e9d4c1e0b4813bf8a481169a8ea6d4ee8e
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-21 11:00:31 -0400

    Created code for sprite movement

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,15 +1,16 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class DrawPanel extends JPanel implements KeyListener {
-    private ArrayList<Johnny> johnny;
+    private Johnny j;
+    private Image sprite;
 
 
     public DrawPanel() {
-        johnny = Johnny.johnBoy();
+        ImageIcon image = new ImageIcon("images/cardboardknife.png");
+        sprite = image.getImage();
 
     }
 
@@ -17,6 +18,10 @@ public class DrawPanel extends JPanel implements KeyListener {
         super.paintComponent(g);
         this.addKeyListener(this);
 
+
+        if (sprite != null) {
+            g.drawImage(sprite,j.getJ_xValue(), j.getJ_yValue(), this);
+        }
 
 
     }
@@ -28,7 +33,18 @@ public class DrawPanel extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            j.moveUserLeft("left");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            j.moveUserRight("right");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            j.moveUserUpwards("up");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            j.moveUserDownwards("down");
+        }
 
     }
 
