063dc806d81b3bbe39483c3a5cc592aa5da2c7e2
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-22 10:55:22 -0400

    Finally figured out why my sprites weren't loading. Will need to make more adjustments tomorrow.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,27 +5,18 @@ public class MainFrame extends JFrame implements Runnable {
     private DrawPanel p;
     private Thread windowThread;
     private JButton b;
-    private Johnny john;
+
 
     public MainFrame(String display) {
         super(display);
-
         int frameWidth = 800;
         int frameHeight = 800;
         p = new DrawPanel();
-        john = new Johnny();
-
-        this.addKeyListener(p);
-        this.add(p);
         JPanel panel = new JPanel();
-        ImageIcon image = new ImageIcon("images/cardboardknife.png");
-        JLabel label = new JLabel(image);
-
         b = new JButton();
-        b.setSize(100, 100);
-        this.add(label);
         this.add(panel);
         panel.add(b);
+        this.add(p);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(frameWidth, frameHeight);
         this.setLocation(500, 10);
