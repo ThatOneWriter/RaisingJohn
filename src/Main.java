892c1ff43cb6a0c0c6dc3be0933cebe48a725958
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-23 11:04:21 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
 
-        String message = "Why is the image not loading. Tell me why..";
+        String message = "Tell me why.. ain't nothing but a (dunno the lyrics) Tell me why, blah blah blah.. I want it that-a-way~";
         long speed = 200;
 
 
