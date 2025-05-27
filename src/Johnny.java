0a0b4f01631f24747264ce51ad997adbe86703a8
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-27 10:52:13 -0400

    Was able to implement Johnny sprite through Johnny and DrawPanel classes instead of MainFrame

--- src/Johnny.java
+++ src/Johnny.java
@@ -1,6 +1,12 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Johnny {
 
     private final String imageFileName;
+    private BufferedImage image;
     private int j_xValue;
     private int j_yValue;
 
@@ -8,15 +14,27 @@ public class Johnny {
     public Johnny() {
         j_xValue = 0;
         j_yValue = 0;
-
         this.imageFileName = "images/chicken.png";
+        getImageFile();
+    }
+
+    public void getImageFile() {
+        try {
+            File imageFile = new File(imageFileName);
+            image = ImageIO.read(imageFile);
+
+        } catch (IOException e) {
+            System.out.print("Error message: Can't find " + imageFileName);
+        }
     }
 
     public String getImageFileName() {
         return imageFileName;
     }
 
-
+    public BufferedImage getImage() {
+        return image;
+    }
 
     public int getJ_xValue() {
         return j_xValue;
@@ -27,18 +45,18 @@ public class Johnny {
     }
 
     public void moveUserRight(String right) {
-        j_xValue++;
+        j_xValue+=10;
     }
 
     public void moveUserLeft(String left) {
-        j_xValue--;
+        j_xValue-=10;
     }
 
     public void moveUserUpwards(String up) {
-        j_yValue++;
+        j_yValue+=10;
     }
 
     public void moveUserDownwards(String down) {
-        j_yValue--;
+        j_yValue-=10;
     }
 }
