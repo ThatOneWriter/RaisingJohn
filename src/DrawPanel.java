c4ad3b82cf89739d9e4966f83c93fdc23357361d
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-29 11:00:37 -0400

    Added the computer image background for the base of the color sorting game

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -6,47 +6,78 @@ import javax.swing.*;
 public class DrawPanel extends JPanel implements KeyListener {
 
     private Johnny johnny;
+    private ComputerBackground computer;
 
     public DrawPanel() {
         johnny = new Johnny();
+        computer = new ComputerBackground();
         this.addKeyListener(this);
     }
 
     protected void paintComponent(Graphics g) {
 
         super.paintComponent(g);
+        if (computer.getBackgroundImage() != null) {
+
+            g.drawImage(computer.getBackgroundImage().getScaledInstance(900, 500, Image.SCALE_SMOOTH), 200, 200, this);
+        }
 
         if (johnny.getImage() != null) {
-
             g.drawImage(johnny.getImage(), johnny.getJ_xValue(), johnny.getJ_yValue(), this);
-
         }
 
 
-
-
-    }
-
-    private void dragImage(JLayeredPane layeredPane, String imagePath, int x, int y) {
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            move(-johnny.getJ_xValue(), 0);
+            System.out.print("Moving left");
+            johnny.moveUserLeft("left");
+
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_D) {
+            move(+johnny.getJ_xValue(), 0);
+            System.out.print("Moving right");
+            johnny.moveUserRight("right");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_W) {
+            move(+johnny.getJ_xValue(), 0);
+            System.out.print("Moving up");
+            johnny.moveUserUpwards("up");
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_S) {
+            move(-johnny.getJ_yValue(), 0);
+            System.out.print("Moving down");
+            johnny.moveUserDownwards("down");
+        }
+        repaint();
+
 
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_A) {
+            move(-johnny.getJ_xValue(), 0);
+            System.out.print("Moving left");
             johnny.moveUserLeft("left");
+
         }
         else if (e.getKeyCode() == KeyEvent.VK_D) {
+            move(+johnny.getJ_xValue(), 0);
+            System.out.print("Moving right");
             johnny.moveUserRight("right");
         }
         else if (e.getKeyCode() == KeyEvent.VK_W) {
+            move(+johnny.getJ_xValue(), 0);
+            System.out.print("Moving up");
             johnny.moveUserUpwards("up");
         }
         else if (e.getKeyCode() == KeyEvent.VK_S) {
+            move(-johnny.getJ_yValue(), 0);
+            System.out.print("Moving down");
             johnny.moveUserDownwards("down");
         }
        repaint();
