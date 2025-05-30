c849bc9f715918b201528213127a17e100e8e970
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-30 10:51:09 -0400

    I added a hitbox for Johnny and began making the color sorting game

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -2,16 +2,26 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class DrawPanel extends JPanel implements KeyListener {
+public class DrawPanel extends JPanel implements KeyListener, MouseListener {
 
     private Johnny johnny;
     private ComputerBackground computer;
+    private Rectangle button;
+
+    private boolean[][] grid;
 
     public DrawPanel() {
         johnny = new Johnny();
         computer = new ComputerBackground();
+        button = new Rectangle(147, 100, 160, 26);
         this.addKeyListener(this);
+        this.addMouseListener(this);
+
+        this.grid = new boolean[10][20];
+
     }
 
     protected void paintComponent(Graphics g) {
@@ -27,6 +37,31 @@ public class DrawPanel extends JPanel implements KeyListener {
         }
 
 
+        johnny.setHitBox(johnny.getJ_xValue(), johnny.getJ_yValue());
+
+        g.setFont(new Font("Monospaced Bold", Font.BOLD, 20));
+        g.drawString("Don't click me", 150, 120);
+
+        int x = 200;
+        int y = 200;
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLUE);
+
+        for (int columns = 0; columns < 20; columns++) {
+            for (int rows = 0; rows < 10; rows++) {
+                g.drawRect(x, y, 20, 20);
+                if (grid[rows][columns]) {
+                    g2.setColor(Color.RED);
+                    g2.fillRect(x, y, 20, 20);
+                    g2.setColor(Color.BLACK);
+                }
+                y += 25;
+            }
+            x += 25;
+            y = 10;
+
+        }
     }
 
     @Override
@@ -86,6 +121,42 @@ public class DrawPanel extends JPanel implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        Point clicked = e.getPoint();
+
+
+        if (e.getButton() == 1) {
+
+            if (button.contains(clicked)) {
+               johnny.isDead();
+            }
+
+
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
