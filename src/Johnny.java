1cca9095429a30617f52e83b65f275219b0f1d24
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-05-30 10:50:37 -0400

    I added a hitbox for Johnny and began making the color sorting game

--- src/Johnny.java
+++ src/Johnny.java
@@ -1,31 +1,46 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+
 public class Johnny {
 
-    private final String imageFileName;
+    private String imageFileName;
     private BufferedImage image;
     private int j_xValue;
     private int j_yValue;
+    private String deadImage;
+    private boolean isDead;
+    private Rectangle hitBox;
 
 
     public Johnny() {
         j_xValue = 1000;
         j_yValue = 500;
         this.imageFileName = "images/chicken.png";
+        this.deadImage = "images/cardboardknife.png";
+        this.isDead = false;
+        this.hitBox = new Rectangle(-100, -100, 200, 281);
         getImageFile();
 
     }
 
-    public void getImageFile() {
+    public BufferedImage getImageFile() {
         try {
             File imageFile = new File(imageFileName);
             image = ImageIO.read(imageFile);
 
+            if (isDead) {
+                File image = new File(deadImage);
+                this.image = ImageIO.read(image);
+            }
+            return image;
+
         } catch (IOException e) {
             System.out.print("Error message: Can't find " + imageFileName);
+            return null;
         }
     }
 
@@ -35,6 +50,11 @@ public class Johnny {
 
     public BufferedImage getImage() {
         return image;
+    }
+
+    public void isDead() {
+        isDead = true;
+        this.image = getImageFile();
     }
 
     public int getJ_xValue() {
@@ -59,5 +79,9 @@ public class Johnny {
 
     public void moveUserDownwards(String down) {
         j_yValue-=10;
+    }
+
+    public void setHitBox(int jXValue, int jYValue) {
+        hitBox.setLocation(jXValue, jYValue);
     }
 }
